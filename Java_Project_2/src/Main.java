de6dc181dcc7ff7f1d27e86c7802ce9ec4d6df69
Piotrek <piotrk05b@gmail.com>
2026-05-08 20:24:12 +0200

    dodanie klasy Beam i zaimplementowanie w niej metody która sprawdza odbicia od scian i przeszkod i zapisuje punkty odbic, jedyny problem moze byc jak wiazka uderzy w sam róg

--- Java_Project_2/src/Main.java
+++ Java_Project_2/src/Main.java
@@ -19,6 +19,12 @@ void main() {
     lineList.add(new Points(10, 100, 10, 100)); // to są przykłady do testowania
     lineList.add(new Points(200, 30, 50, 120));
 
+    Beam beam = new Beam();
+    Beam Top = new Beam();
+    beam.calc_points(30, 10, 3, 4, obstacleList, lineList, ScreenSizeX, ScreenSizeY, 3);
+    Top.calc_points(30, 430, 2, -2.5, obstacleList, lineList, ScreenSizeX, ScreenSizeY, 2);
+
+
     LineDrawing d = new LineDrawing(lineList);
     Screen S = new Screen(ScreenSizeX,ScreenSizeY, d);
     S.paintObstacles(obstacleList);
@@ -38,23 +44,23 @@ class Screen extends JFrame {
         for (Points o : obs){
             JPanel k = new JPanel();
             k.setBackground(Color.BLACK);
-            k.setBounds(o.x0, o.y0, Math.abs(o.x1-o.x0),Math.abs(o.y1-o.y0));
+            k.setBounds(o.x1, o.y1, Math.abs(o.x2-o.x1),Math.abs(o.y2-o.y1));
             this.getContentPane().add(k);
         }
     }
 }
 
 class Points {
-    int x0;
     int x1;
-    int y0;
     int y1;
+    int x2;
+    int y2;
 
-    public Points(int x0, int x1, int y0, int y1) {
-        this.x0 = x0;
+    public Points(int x1, int y1, int x2, int y2) {
         this.x1 = x1;
-        this.y0 = y0;
         this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
     }
 }
 
@@ -71,7 +77,97 @@ class LineDrawing extends JComponent{
         super.paintComponent(g);
         for (Points l : list) {
             g.setColor(Color.RED);
-            g.drawLine(l.x0, l.y0, l.x1, l.y1);
+            g.drawLine(l.x1, l.y1, l.x2, l.y2);
         }
     }
+}
+
+class Beam {
+    public void calc_points(double startX, double startY, double velX, double velY,
+                                  ArrayList<Points> obstacles, ArrayList<Points> resultLines,
+                                  int screenX, int screenY, int maxBounces) {
+// dałem double a nie int bo mogły być błędy przy małych prędkościach
+
+
+        double currentX = startX;
+        double currentY = startY;
+
+        // Punkt z którego wiązka wyleciała po ostatnim odbiciu
+        double lastBounceX = startX;
+        double lastBounceY = startY;
+
+        int bounceCount = 0;
+
+
+        while (bounceCount <= maxBounces) {
+            double prevX = currentX;
+            double prevY = currentY;
+
+            currentX += velX;
+            currentY += velY;
+
+            boolean bounced = false;
+
+            // Sprawdzanie granic ekranu
+            if (currentX <= 0 || currentX >= screenX) {
+                velX = -velX;
+                currentX = (currentX <= 0) ? 0 : screenX;
+                bounced = true;
+            }
+            if (currentY <= 0 || currentY >= screenY) {
+                velY = -velY;
+                currentY = (currentY <= 0) ? 0 : screenY;
+                bounced = true;
+            }
+
+            // Sprawdzanie odbic od przeszkod
+            if (!bounced) {
+                for (Points obs : obstacles) {
+                    int left = Math.min(obs.x1, obs.x2);
+                    int right = Math.max(obs.x1, obs.x2);
+                    int top = Math.min(obs.y1, obs.y2);
+                    int bottom = Math.max(obs.y1, obs.y2);
+
+                    if (currentX >= left && currentX <= right && currentY >= top && currentY <= bottom) {
+
+                        // Sprawdzenie strony, z której wlecial
+                        if (prevX < left || prevX > right) {
+                            velX = -velX;
+                            currentX = (prevX < left) ? left : right;
+                        }
+                        else {
+                            velY = -velY;
+                            currentY = (prevY < top) ? top : bottom;
+                        }
+
+                        bounced = true;
+                        break;
+                    }
+                }
+            }
+
+            // Zapisywanie odbicia do listy
+            if (bounced) {
+                resultLines.add(new Points((int)lastBounceX, (int)lastBounceY, (int)currentX, (int)currentY));
+                lastBounceX = currentX;
+                lastBounceY = currentY;
+                bounceCount++;
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
